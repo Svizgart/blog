b5f0bfb23d81753b3f428f5a038e8997e37c8dea
Aleksandr.Saiko <aleksandr.cayko@realize-it.ru>
2018-09-26 16:29:43 +0300

    добвалена ввторизация инкопсулированы стр для админа

---
+++ src/Controllers/AutController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aleksandr.cayko
+ * Date: 26.09.2018
+ * Time: 11:46
+ */
+
+namespace Controllers;
+
+
+class AutController
+{
+
+}
---
+++ src/form_aut.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <?php include_once 'header.html'; ?>
+</head>
+<body>
+
+
+<div class="">
+
+</div>
+<?php  ?>
+
+</body>
+</html>
